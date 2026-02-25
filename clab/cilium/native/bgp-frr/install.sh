6f5db0360b472e77710ad87d7bfc253a70f9f537
Yunkang Ren <rykren1998@gmail.com>
2026-02-25 14:14:31 +0800

    Update install.sh

--- clab/cilium/native/bgp-frr/install.sh
+++ clab/cilium/native/bgp-frr/install.sh
@@ -5,3 +5,9 @@ cilium install \
     --set ipv4NativeRoutingCIDR="10.0.0.0/8" \
     --set bgpControlPlane.enabled=true \
     --set k8s.requireIPv4PodCIDR=true
+
+cilium status --wait
+cilium config view | grep enable-bgp
+
+docker exec -it clab-bgp-topo-tor0 vtysh -c 'show bgp ipv4 summary wide'
+docker exec -it clab-bgp-topo-tor1 vtysh -c 'show bgp ipv4 summary wide'
